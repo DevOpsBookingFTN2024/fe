04762aa03d1f52a08a48200974939db6c0a074d6
Branislav Roljic <branislavroljic99@gmail.com>
2024-12-05 03:09:48 +0100

    feat: integrate mui theme and setup ui components. Create Login and Home pages.

---
+++ app/src/layouts/full/vertical/header/Navigation.tsx
@@ -0,0 +1,14 @@
+import {Stack} from "@mui/material";
+
+const AppDD = () => {
+  return (
+      <Stack
+          direction={"row"}
+          gap={1}
+          justifyContent={"center"}
+          alignItems={"center"}
+      ></Stack>
+  );
+};
+
+export default AppDD;
---
+++ app/src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+//   const { data: pdfUrl, isLoading } = useQuery({
+//     queryKey: ["document", selectedDocumentType, selectedDocument],
+//     queryFn: async () => {
+//       if (selectedDocument != undefined)
+//         return getFile(selectedDocumentType, selectedDocument + ".pdf");
+//     },
+//     enabled: !!selectedDocument,
+//   });
+
+return (<div>tu samm</div>)
+}
